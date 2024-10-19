bedcde2a855a406d8f04a48b95d341dc4e281024
Heeyeong000 <gmldud6128@naver.com>
2024-10-19 18:20:46 +0900

    week 7

--- week07/app.js
+++ week07/app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', require('./routes/users'));
+
 // kakao는 routes의 kakao 사용
 app.use('/kakao', require('./routes/kakao'));
 
