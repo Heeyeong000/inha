c4a6dc83d8847500d47f5ae47e2a213da8c3447f
Heeyeong000 <59953401+Heeyeong000@users.noreply.github.com>
2024-12-14 16:42:17 +0900

    week14 munebar 추가

---
+++ week14/routes/bbs.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* 게시판 목록 */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: '게시판', pageName:'bbs/list.ejs' });
+});
+
+/* 글쓰기 */
+router.get('/insert', function(req, res, next) {
+    res.render('index', { title: '글쓰기', pageName:'bbs/insert.ejs' });
+});
+
+/* 게시글 정보페이지 */
+router.get('/:id', function(req, res, next) {
+    const id=req.params.id;
+    // console.log('id.................', id)
+
+    res.render('index', { title: '게시글 정보', pageName:'bbs/read.ejs', id });
+});
+
+/* 게시글 수정페이지 */
+router.get('/update/:id', function(req, res, next) {
+    const id=req.params.id;
+    // console.log('id.................', id)
+    res.render('index', { title: '게시글 수정', pageName:'bbs/update.ejs', id });
+});
+
+module.exports = router;
